2289ef3a9e925dd0f2a3c99f912bc4488e05987a
tharun sai <kk6568@srmist.edu.in>
2026-03-12 15:23:35 +0530

    feature/uc2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,11 @@
-public class OOPSBannerApp{
-public static void main(string[] args){
-System.out.println(x:"OOPS");
-}
+class OopsWord {
+    public static void main(String[] args) {
+    System.out.println("  ***  "+"  ***  "+"******"+" ****** ");
+	System.out.println(" *   * "+" *   * "+"*    *"+" *      ");  
+	System.out.println(" *   * "+" *   * "+"*    *"+" *      ");
+	System.out.println(" *   * "+" *   * "+"******"+" ****** ");
+	System.out.println(" *   * "+" *   * "+"*     "+"      * ");
+	System.out.println(" *   * "+" *   * "+"*     "+"      * ");
+	System.out.println("  ***  "+"  ***  "+"*     "+" ****** "); 
+    }
 }
